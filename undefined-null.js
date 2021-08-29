a92f7bf437e3b01bfc2e0db1ac97969ca8fbac4c
unknown <mobinh264@gmail.com>
2021-08-30 02:42:55 +0600

    Some code are changing

--- undefined-null.js
+++ undefined-null.js
@@ -1,102 +1,20 @@
-/* 
-1. variable value not assign
-2. function but didn't write return anything
-3. jast wrote return but didn't return anything 
-4. parameter that isn't passed
-5. proparty doesn't exits in an object
-6. access in array element out of range
-7. accessing deleted array element
-8. Explicitly set value to undefined
-
-*/
-
-// 1.st case undifine
-let fist;
-console.log(fist);
-
-//2. case undifine
-
-function second(x, y){
-    const sum = x + y;
+const first = '0';
+const second = false;
+if (first == second) {
+    console.log('condition is true');
 }
-const result = second( 3, 91);
-console.log(result);
-
-// 3 case
-function second(x, y){
-    // document.getElementById('result');
-    
-}
-const resultTwo = second( 3, 91);
- console.log(resultTwo);
-
-
-// 4. case 
-
-function num( x, y ){
-    const resut = x + y;
-    return;
-
-    const fun = x * y;
-    return sum;
+else {
+    console.log('condition is false');
 }
 
-const output = num( 5, 6);
-console.log(output)
-
-// 5. case
-function num( x, y ){
-    const resut = x + y;
-    
-    if( x < 10){
-        return;
-    }
-    const fun = x * y;
-    return sum;
+//more comparison
+// const a = { name: 'ali' };
+// const b = { name: 'ali' };
+const a = [];
+const b = [];
+if (a === b) {
+    console.log('both are same')
 }
-
-const fourth = num( 2, 7 );
-console.log(fourth);
-
-// 6.case 
-function double (a, b){
-    const result = a * 2;
-    console.log(b);
-    return result;
+else {
+    console.log('they are not same')
 }
-//  double(81);
-
-// 7.  case 
-const student = {
-    name : "Fuhad",
-    maritualStatus : true,
-    address : 'kalsi',
-    job: 'tea stall'
-}
-
-console.log( student.education);
-
-// 8. case 
-const arrayUndif = [ 22, 54, 57, 52, 34, 96, 4 ];
-
- console.log( arrayUndif[9]);
-
-const arrayUndif2 = [ 22, 54, 57, 52, 34, 96, 4, 524, 2 ];
-
-// delete arrayUndif2[4];
-// console.log( arrayUndif2); //[ 22, 54, 57, 52, <1 empty item>, 96, 4, 524, 2 ]
-// console.log(arrayUndif2[4]);
-
-
-// 9. Explicitly set undefined any how 
-
-const nineth = undefined;
-console.log(nineth)
-
-
-
-/********************************/
-        //NULL
-/********************************/
-
-const nyObj = { name: 'Samad', profession: null};
