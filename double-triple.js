c0dff9e2b5290e50c6aaab80e585880468a42fce
unknown <mobinh264@gmail.com>
2021-08-30 02:40:25 +0600

    doubble triple equal defarance between

---
+++ double-triple.js
@@ -0,0 +1,19 @@
+const fist = 2;
+const second = 2;
+
+if( fist == second ){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false")
+}
+
+const fist = 2;
+const second = 2;
+
+if( fist == second ){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false")
+}
